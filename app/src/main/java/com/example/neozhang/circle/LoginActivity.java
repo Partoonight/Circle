33234f287544ec65066bb1cf4ae86d4bd71ab57a
NeoZhangTCL <ztuochao@uwo.ca>
2015-11-28 13:36:48 -0500

    a facebook login & slides
    you need a facebook developer account

---
+++ app/src/main/java/com/example/neozhang/circle/LoginActivity.java
@@ -0,0 +1,67 @@
+package com.example.neozhang.circle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+
+
+        View view = findViewById(R.id.root_login_view);
+
+        loginButton = (LoginButton) view.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+        // Other app specific specialization
+
+        callbackManager = CallbackManager.Factory.create();
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Log.d("", "successfully logged in");
+//transition to main activity here+++
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Log.d("", "cancelled login");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Log.d("", "failed login");
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
