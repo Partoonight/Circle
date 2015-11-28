2280f6037da745dea5dd1046ae30431d7700770d
Jyjhuang <Jeremy.yj.huang@gmail.com>
2015-11-28 18:01:52 -0500

    user model and BL classes

---
+++ app/src/main/java/com/example/neozhang/circle/userRepository.java
@@ -0,0 +1,16 @@
+package com.example.neozhang.circle;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Huang on 11/28/2015.
+ */
+public class userRepository {
+    user newUser;
+    public userRepository(user newUser)
+    {
+        this.newUser = newUser;
+    }
+
+
+}
